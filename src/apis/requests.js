d62bd66dc4ce01e56edf24fa222f935acdb48366
pras306 <44067654+pras306@users.noreply.github.com>
2023-03-28 21:49:24 +0530

    MODIFY API Proxy Server URL
    - Moved server from railway app to render app
    - Made changes in README to show the necessary changes as well

--- src/apis/requests.js
+++ src/apis/requests.js
@@ -1,4 +1,4 @@
-export const CLARIFAI_BASE_URL = "https://api-proxy-server.up.railway.app/api/v1/images/detect";
+export const CLARIFAI_BASE_URL = "https://api-proxy-server-6xbe.onrender.com/api/v1/images/detect";
 
 export const CLARIFAI_MODELS = {
     faceModel: "/face",
